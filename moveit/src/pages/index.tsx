7f273f3d5454b974332cc0ce2caf36e539169af4
danyAmaral <danyamaral27@gmail.com>
2021-02-23 20:47:37 -0300

    criando componentes countdown e de completedChallages

--- moveit/src/pages/index.tsx
+++ moveit/src/pages/index.tsx
@@ -1,4 +1,7 @@
+import { Head } from 'next/document';
 import React from 'react';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 
@@ -6,14 +9,19 @@ import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
     return (
-        <div className={styles.container}>
-        <ExperienceBar></ExperienceBar>
-        <section>
-          <div>
-            <Profile></Profile>
-          </div>
-          <div></div>
-        </section>
-      </div>
+          <div className={styles.container}>
+          {/* <Head>
+            <title>Início | move.it</title>
+          </Head> */}
+          <ExperienceBar />
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div></div>
+          </section>
+        </div>
     )
 }
