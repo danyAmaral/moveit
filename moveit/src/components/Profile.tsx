c746ea221c9fd8b71016ee6441693b64d43ba6d4
danyAmaral <danyamaral27@gmail.com>
2021-02-23 19:49:17 -0300

    criando  projeto com next.js e componentes de ExperienceBar e Profile

---
+++ moveit/src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img 
+                src="https://github.com/danyamaral.png" 
+                alt="Danielle Amaral">
+            </img>
+            <div>
+                <strong>Danielle Amaral</strong>
+                <p>
+                  <img src="icons/level.svg" alt="Level"></img>  
+                    Level 1</p>
+            </div>
+        </div>
+    );
+}
